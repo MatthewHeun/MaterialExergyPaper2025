448631f1743f0996e0cd817a89f31c0b35a17684
Matthew Heun <matthew.heun@me.com>
2025-06-26 15:52:00 -0400

    Working on ZAF ecc information

--- data/energy_data.R
+++ data/energy_data.R
@@ -24,23 +24,21 @@ conn <- DBI::dbConnect(drv = RPostgres::Postgres(),
 on.exit(DBI::dbDisconnect(conn))
 
 # Read the data once
-psut_zaf_2013 <- PFUPipelineTools::pl_filter_collect("PSUTReAllChopAllDsAllGrAll",
-                                                     Dataset == "CL-PFU IEA",
-                                                     Country == "ZAF",
-                                                     Year == 2013,
-                                                     LastStage == "Final",
-                                                     IncludesNEU == TRUE,
-                                                     ProductAggregation == "Despecified",
-                                                     IndustryAggregation == "Despecified",
-                                                     conn = conn,
-                                                     collect = TRUE,
-                                                     matrix_class = "matrix") |>
+psut_io_zaf_2013 <- PFUPipelineTools::pl_filter_collect("PSUTReAllChopAllDsAllGrAll",
+                                                        Dataset == "CL-PFU IEA",
+                                                        Country == "ZAF",
+                                                        Year == 2013,
+                                                        LastStage == "Final",
+                                                        IncludesNEU == FALSE,
+                                                        ProductAggregation == "Despecified",
+                                                        IndustryAggregation == "Despecified",
+                                                        conn = conn,
+                                                        collect = TRUE,
+                                                        matrix_class = "matrix") |>
   dplyr::mutate(
     Dataset = factor(Dataset, levels = c("CL-PFU IEA", "CL-PFU MW", "CL-PFU IEA+MW"))
   ) |>
-  dplyr::arrange(Country, Year, EnergyType, LastStage, IncludesNEU, Dataset)
-
-psut_zaf_2013_io <- psut_zaf_2013 |>
+  dplyr::arrange(Country, Year, EnergyType, LastStage, IncludesNEU, Dataset) |>
   Recca::calc_io_mats()
 
 #
@@ -50,19 +48,54 @@ psut_zaf_2013_io <- psut_zaf_2013 |>
 # Read energy and exergy requirements
 # from the material conversion chain spreadsheet.
 
-new_Y <- openxlsx2::read_xlsx(file = file.path("data", "Paper Examples 3.xlsx"),
-                              named_region = "mcc_energy_reqts") |>
-  dplyr::select(EnergyCarrier, `E [TJ]`, `X [TJ]`)
+mcc_energy_reqts <- openxlsx2::read_xlsx(file = file.path("data", "Paper Examples 3.xlsx"),
+                                         named_region = "mcc_energy_reqts") |>
+  dplyr::select(EnergyCarrier, `E [TJ]`, `X [TJ]`) |>
+  dplyr::rename(E = "E [TJ]", X = "X [TJ]", rownames = "EnergyCarrier") |>
+  tidyr::pivot_longer(cols = c("E", "X"),
+                      names_to = "EnergyType",
+                      values_to = "matvals") |>
+  dplyr::mutate(
+    colnames = "Iron and steel",
+    rowtypes = "Product",
+    coltypes = "Industry"
+  ) |>
+  dplyr::group_by(EnergyType) |>
+  matsindf::collapse_to_matrices() |>
+  dplyr::rename(Y_prime = "matvals")
+
+#
+# Build a new energy conversion chain that
+# supplies exactly the amount of energy
+# needed for the material conversion chain
+
+ecc <- dplyr::left_join(psut_io_zaf_2013, mcc_energy_reqts, by = "EnergyType") |>
+  Recca::new_Y() |>
+  dplyr::mutate(
+    R = NULL,
+    U = NULL,
+    V = NULL,
+    Y = NULL,
+    U_EIOU = NULL,
+    U_feed = NULL,
+    r_EIOU = NULL
+  ) |>
+  dplyr::rename(
+    R = R_prime,
+    U = U_prime,
+    V = V_prime,
+    Y = Y_prime,
+    U_EIOU = U_EIOU_prime,
+    U_feed = U_feed_prime,
+    r_EIOU = r_EIOU_prime
+  ) |>
+  dplyr::select(Dataset, ValidFromVersion, ValidToVersion, Country, Method, EnergyType, LastStage,
+                IncludesNEU, Year, R, U, V, Y, U_EIOU, U_feed, r_EIOU, S_units)
+
+ecc |>
+  Recca::write_ecc_to_excel(path = "data/energy_ecc.xlsx", overwrite_file = TRUE)
 
 
-
-
-
-
-# To read this file in R, use code like the following:
-foo <- readRDS("~/Desktop/psut_fra_2020_for_baptiste.rds")
-# To look at the matrices, use code like the following in the Console of RStudio:
-foo$R[[1]] |> View()
 
 
 
