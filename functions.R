11b9b2fc50488b6f60b38a7bda115d4e202067ed
Matthew Heun <matthew.heun@me.com>
2025-07-28 16:25:27 -0400

    Now adding cell colors

--- functions.R
+++ functions.R
@@ -27,26 +27,39 @@ read_named_cell <- function(cell_name,
 # Default is digits = 2.
 print_named_matrix <- function(matrix_name,
                                latex_label,
-                               file = file.path("data", "Example.xlsx"),
+                               file = file.path("data", "Paper Examples.xlsx"),
                                digits = 2,
                                caption = NULL,
-                               size = "\\normalsize") {
+                               size = "normalsize") {
   # Read the matrix as a data frame
   mat_df <- openxlsx2::read_xlsx(file = file,
                                  named_region = matrix_name,
                                  row_names = TRUE) |>
+    # Add a background colour to cells in every column
+    dplyr::mutate(
+      dplyr::across(dplyr::everything(),
+                    ~ ifelse(is.na(.),
+                             # When NA, add just color
+                             "\\cellcolor{gray!20} ",
+                             # When not NA, include the value
+                             paste0("\\cellcolor{gray!20} ", .)
+                    )
+      )
+    ) |>
     tibble::rownames_to_column(var = " ")
+
 
   # Create an xtable from the matrix
   xt <- xtable::xtable(mat_df,
                        digits = digits,
                        caption = caption,
-                       align = rep("c", ncol(mat_df) + 1),
+                       align = c("r", "r", rep("c", ncol(mat_df)-1)),
                        label = latex_label)
   # Print the matrix as a xtable
   print(xt,
         include.rownames = FALSE,
         # sanitize.colnames.function = identity,
+        sanitize.text.function = identity,
         rotate.colnames = TRUE,
         hline.after = NULL,
         floating.environment = "figure",
